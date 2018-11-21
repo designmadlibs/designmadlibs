f173ae6514b463d3a765cbee0c8a5acdc6a97c36
Alexa Kaminsky <akaminsky9@gmail.com>
2018-11-21 00:40:01 -0500

    make the js work for about page on mobile

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 
     var pageURL = $(location).attr("href");
@@ -18,24 +17,37 @@ $(document).ready(function(){
 		    }, 1000);
 		}
 	});
+
+
+	$('.mobile-nav').on("change", function () {
+		var className=$(this).val();
+
+		if(pageURL.includes("about")){
+			//var target = $('#'+className)
+			window.location.replace("index.html#" +className);
+		}
+		else {
+			var target = $('#'+className)
+			$('html, body').animate({
+		      scrollTop: target.offset().top
+		    }, 1000);
+		}
+	});
+
+
+	$('.back-to-top').on("click", function() {
+		var target = $('.logo')
+	    $('html, body').animate({
+	      scrollTop: target.offset().top
+	    }, 1000);
+	});
+
 });
 
-$('.back-to-top').on("click", function() {
-	var target = $('.logo')
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-});
 
 
-$('.mobile-nav').on("change", function () {
-	var className=$(this).val();
 
-	var target = $('#'+className)
-    $('html, body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-});
+
 
 
 
